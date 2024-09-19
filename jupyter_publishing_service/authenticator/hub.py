efeb55d7880f397c71058323c241e26aac9f3cfa
Zach Sailer <zsailer@apple.com>
2024-09-18 22:20:54 -0400

    Add methods to fetch tokens

--- jupyter_publishing_service/authenticator/hub.py
+++ jupyter_publishing_service/authenticator/hub.py
@@ -3,6 +3,7 @@ from traitlets import Unicode
 from traitlets.config import LoggingConfigurable
 
 from jupyter_publishing_service.constants import JUPYTERHUB_SCOPE
+from jupyter_publishing_service.models.sql import User
 
 from .abc import AuthenticatorABC
 
@@ -11,15 +12,15 @@ class HubAuthenticator(LoggingConfigurable):
 
     hub_url = Unicode(default_value="http://localhost:8000").tag(config=True)
 
-    async def authenticate(self, user_token):
+    async def authenticate(self, token: str) -> User:
         user_endpoint = self.hub_url + "/hub/api/user"
-        auth_header = {"Authorization": f"Bearer {user_token['token']}"}
+        auth_header = {"Authorization": f"Bearer {token}"}
         async with AsyncClient(verify=False) as client:
             r = await client.get(user_endpoint, headers=auth_header)
             user_model = r.json()
             user_scopes = user_model["scopes"]
             assert JUPYTERHUB_SCOPE in user_scopes
-            return user_model
+            return User(username=user_model.get("username"))
 
 
 AuthenticatorABC.register(HubAuthenticator)
